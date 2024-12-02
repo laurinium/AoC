30f7b45e3b481a0e23ae8ae4281625e92b7151d4
Jens Laur <laur@secra.de>
2024-12-02 17:30:06 +0100

    addiditons

--- puzzles/2024/2.go
+++ puzzles/2024/2.go
@@ -4,7 +4,6 @@ import (
 	"adventOfCode/puzzles"
 	"database/sql"
 	"fmt"
-	"sort"
 	"strconv"
 	"strings"
 )
@@ -17,72 +16,72 @@ var day2 = puzzles.NewPuzzle(2024, 2, func(input string) {
 
 	lines := strings.Split(input, "\n")
 	var count = 0
+	var lineIsValid = true
+	var badLevels = 0
 	for _, line := range lines {
 
 		var columns = strings.Split(line, " ")
-		var prevValue *int
-		prevValue = nil
+		var prevValue sql.NullInt64
+		prevValue = sql.NullInt64{}
+		var prevDir sql.NullBool
+		prevDir = sql.NullBool{}
 		for _, column := range columns {
-			value1, _ := strconv.Atoi(column)
-			if prevValue != nil {
-				distance := value1 - *prevValue
+			value1, _ := strconv.ParseInt(column, 10, 64)
+			var dir = sql.NullBool{}
+			if prevValue.Valid {
+				// check if the direction is the same as the previous
+				if value1 > prevValue.Int64 {
+					dir.Bool = true
+				} else {
+					dir.Bool = false
+				}
+				dir.Valid = true
+				if prevDir.Valid {
+					if dir.Bool != prevDir.Bool {
+						// break both loops
+						fmt.Println("Direction Error: ", columns)
+						lineIsValid = false
+						badLevels++
+					}
+				}
+				// check if the distance is less than 3 and greater than 0
+				distance := prevValue.Int64 - value1
 				if distance < 0 {
 					distance = distance * -1
-					if distance > 1 {
-						fmt.Println("Invalid")
-						return
-
-					}
-					count++
 				}
-
+				if distance <= 0 || distance > 3 {
+					lineIsValid = false
+					badLevels++
+					fmt.Println("Distance Error: ", columns)
+				}
+				prevDir.Bool = dir.Bool
+				prevDir.Valid = true
 			}
-			prevValue = value1
+			// set the previous value
+			prevValue.Int64 = value1
+			prevValue.Valid = true
+			// last column
 		}
-	}
-
-	//sort the arrays
-	sort.Ints(list1)
-	sort.Ints(list2)
-
-	var distances = []int{}
-	// loop through the arrays
-	for i := 0; i < len(list1); i++ {
-		distances = append(distances, list2[i]-list1[i])
-	}
-
-	//sum the distances
-	var sum = 0
-	for _, distance := range distances {
-		if distance < 0 {
-			distance = distance * -1
-		}
-		sum += distance
-	}
-	fmt.Println(sum)
-
-	var simScores = []int{}
-	//part 2
-	for i := 0; i < len(list1); i++ {
-		var num1 = list1[i]
-		// count the number of times num1 appears in list2
-		var count = 0
-		for j := 0; j < len(list2); j++ {
-			if list2[j] == num1 {
+		if !lineIsValid {
+			if badLevels == 1 {
 				count++
+			} else {
+				fmt.Println("Too many errors", badLevels)
 			}
 		}
-		simScores = append(simScores, num1*count)
-	}
 
-	//sum the simScores
-	var simSum = 0
-	for _, simScore := range simScores {
-		simSum += simScore
+		if lineIsValid {
+			count++
+		} else {
+			fmt.Println("Invalid line")
+		}
+		lineIsValid = true
+		badLevels = 0
+		//os.Exit(0)
 	}
-	fmt.Println(simSum)
+	fmt.Println(count)
 })
 
 func init() {
-	puzzles.RegisterPuzzle(day1)
+	puzzles.RegisterPuzzle(day2)
 }
